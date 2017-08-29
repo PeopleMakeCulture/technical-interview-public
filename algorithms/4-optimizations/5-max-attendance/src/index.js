4767b01a1eaa24df847329a1f46f22dfd36842ee
Omri Bernstein <hippiccolo@gmail.com>
2017-08-29 10:33:40 -0400

    feat: get d3 multi select working

--- algorithms/4-optimizations/5-max-attendance/src/index.js
+++ algorithms/4-optimizations/5-max-attendance/src/index.js
@@ -1,4 +1,13 @@
-import * as d3 from 'd3';
+// import 'd3-selection-multi';
+// import * as d3 from 'd3';
 
-console.log('d3', d3);
+const width = 500;
+const height = 500;
+
+const svg = window.d3.select('#root')
+.append('svg')
+.attrs({
+  width,
+  height
+});
 
